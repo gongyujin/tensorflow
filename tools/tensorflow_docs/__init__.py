bd3f7aafd39368598e3c5a5ed918908a3a23c9ce
gongyujin <96815817+gongyujin@users.noreply.github.com>
2022-08-28 22:15:38 +0900

    Update __init__.py

--- tools/tensorflow_docs/__init__.py
+++ tools/tensorflow_docs/__init__.py
@@ -18,4 +18,4 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow_docs import api_generator
+from tensorflow_docs import vis
